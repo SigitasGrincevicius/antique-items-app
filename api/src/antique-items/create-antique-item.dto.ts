87d8d09af8de87749d918edb246864fef766cb28
Sigitas <s.grincevicius@gmail.com>
2026-05-30 16:37:24 +0300

    feat(api): update example for creating antique items with correct name and origin

--- api/src/antique-items/create-antique-item.dto.ts
+++ api/src/antique-items/create-antique-item.dto.ts
@@ -1,6 +1,19 @@
+import { IsNotEmpty, IsNumber, isString, IsString } from 'class-validator';
+
 export class CreateAntiqueItemDto {
+  @IsNotEmpty()
+  @IsString()
   name!: string;
+
+  @IsNotEmpty()
+  @IsString()
   origin!: string;
+
+  @IsNotEmpty()
+  @IsNumber()
   year!: number;
+  
+  @IsNotEmpty()
+  @IsNumber()
   priceEur!: number;
 }
