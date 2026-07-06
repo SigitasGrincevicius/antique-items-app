f4ef3ce9304670b4966eb4b8f88c91ae62137f01
Sigitas <s.grincevicius@gmail.com>
2026-07-06 12:16:18 +0300

    feat(auth): implement login method with user verification and token generation

--- api/src/users/auth/auth.service.ts
+++ api/src/users/auth/auth.service.ts
@@ -1,17 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { CreateUserDto } from '../create-user.dto';
 import { UsersService } from '../users/users.service';
 import { User } from '../user.entity';
+import { PasswordService } from '../password/password.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly usersService: UsersService,
     private readonly jwtService: JwtService,
+    private readonly passwordService: PasswordService,
   ) {}
 
   public async register(createUserDto: CreateUserDto) {}
+
+  public async login(email: string, password: string): Promise<string> {
+    const user = await this.usersService.findOneByEmail(email);
+
+    // There is no such user
+    if (!user) {
+      throw new UnauthorizedException('Invalid creadentials');
+    }
+
+    // Password is invalid
+    if (!(await this.passwordService.verify(password, user.password))) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    return this.generateToken(user);
+  }
 
   private generateToken(user: User): string {
     const payload = { sub: user.id, name: user.name };
