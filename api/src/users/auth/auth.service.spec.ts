69bb5b1c6e9a55ec0159a385ded6af9498fbf37b
Sigitas <s.grincevicius@email.com>
2026-07-07 20:34:35 +0300

    feat(tests): enhance AuthService and UsersService tests with mock providers

--- api/src/users/auth/auth.service.spec.ts
+++ api/src/users/auth/auth.service.spec.ts
@@ -1,12 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { JwtService } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
+import { PasswordService } from '../password/password.service';
+import { UsersService } from '../users/users.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
+  const usersServiceMock = {
+    createUser: jest.fn(),
+    findOneByEmail: jest.fn(),
+  };
+
+  const jwtServiceMock = {
+    sign: jest.fn(),
+  };
+
+  const passwordServiceMock = {
+    verify: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: usersServiceMock,
+        },
+        {
+          provide: JwtService,
+          useValue: jwtServiceMock,
+        },
+        {
+          provide: PasswordService,
+          useValue: passwordServiceMock,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
--- api/src/users/users/users.service.spec.ts
+++ api/src/users/users/users.service.spec.ts
@@ -1,12 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
+import { User } from '../user.entity';
+import { PasswordService } from '../password/password.service';
 
 describe('UsersService', () => {
   let service: UsersService;
 
+  const userRepositoryMock = {
+    findOneBy: jest.fn(),
+    save: jest.fn(),
+  };
+
+  const passwordServiceMock = {
+    hash: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: userRepositoryMock,
+        },
+        {
+          provide: PasswordService,
+          useValue: passwordServiceMock,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
