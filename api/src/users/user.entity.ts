5d21984c321495a3984fe54e9fc9428e94eb57a2
Sigitas <s.grincevicius@gmail.com>
2026-07-10 09:44:48 +0300

    feat(auth): update user registration test data and enhance serialization options

--- api/src/users/user.entity.ts
+++ api/src/users/user.entity.ts
@@ -6,12 +6,13 @@ import {
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
-import { Exclude } from 'class-transformer';
+import { Exclude, Expose } from 'class-transformer';
 import { AntiqueItem } from '../antique-items/antique-item.entity';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn('uuid')
+  @Expose()
   id!: string;
 
   @Column({
@@ -19,9 +20,9 @@ export class User {
     length: 255,
     nullable: false,
   })
+  @Expose()
   name!: string;
 
-  @Exclude()
   @Column()
   password!: string;
 
@@ -30,14 +31,18 @@ export class User {
     length: 255,
     unique: true,
   })
+  @Expose()
   email!: string;
 
   @CreateDateColumn()
+  @Expose()
   createdAt!: Date;
 
   @UpdateDateColumn()
+  @Expose()
   updatedAt!: Date;
 
   @OneToMany(() => AntiqueItem, (item) => item.createdBy)
+  @Expose()
   antiqueItems!: AntiqueItem[];
 }
