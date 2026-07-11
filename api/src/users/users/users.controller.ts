46e103b672746c6323a20298ffedf997c3f9c76c
Sigitas <s.grincevicius@email.com>
2026-07-11 20:22:45 +0300

    refactor(users): reorganize imports and add serialization options for users controller

--- api/src/users/users/users.controller.ts
+++ api/src/users/users/users.controller.ts
@@ -1,10 +1,17 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  SerializeOptions,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from '../user.entity';
 import { FindOneParams } from '../../antique-items/find-one.params';
-import { CreateUserDto } from '../create-user.dto';
 
 @Controller('users')
+@SerializeOptions({ strategy: 'excludeAll' })
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
